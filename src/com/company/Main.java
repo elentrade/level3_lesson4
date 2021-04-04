548de4816421454c0f12846981b04273c446c54d
Elena <elentrade@rambler.ru>
2021-04-04 21:57:19 +0400

    папка ex1 - ABC, ex2 - MFU

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,23 +3,21 @@ package com.company;
 import java.io.File;
 
 public class Main {
-    char l;
-public static class Litter {
-    public  synchronized void printLitter(char l) {
-            for (int i = 0; i < 5; i++) {
-                System.out.println(l);
-                try {
-                    wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+public static class Litter{
+    public synchronized void printLitter(char l) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(l);
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+        }
     }
 }
 
     public static void main(String[] args) {
     Litter litter = new Litter();
-
         Thread tr1 = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -38,10 +36,10 @@ public static class Litter {
                 litter.printLitter('C');
             }
         });
-
         tr1.start();
         tr2.start();
         tr3.start();
+
 
     }
 }
